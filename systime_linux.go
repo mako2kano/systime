a0fb33f396ceeeb51adc8474628a0a7cc1989276
Makoto Tokano <makoto.tokano@hakusan.co.jp>
2018-08-01 11:14:31 +0900

    Add: SetLocalTime for linux

---
+++ systime_linux.go
@@ -0,0 +1,22 @@
+// Copyright 2018 Makoto Tokano.
+// Use of this source code is governed by a zlib-style
+// license that can be found in the LICENSE file.
+
+package systime
+
+import (
+	"syscall"
+	"time"
+)
+
+type linuxSysTime struct{}
+
+func (linuxSysTime) SetLocalTime(t time.Time) error {
+
+	tv := syscall.NsecToTimeval(t.UnixNano())
+	return syscall.Settimeofday(&tv)
+}
+
+func init() {
+	chooseSysTM(linuxSysTime{})
+}
